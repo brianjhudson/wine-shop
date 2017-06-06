ef76e4b7067b7c6ffee95f6019d02fd8fc08e385
Brian Hudson <brianjasonhudson@gmail.com>
2017-06-05 21:49:06 -0500

    Moving port to .env and reinstalling axios

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
 app.use(express.static(__dirname + '/dist'))
 app.get('*', express.static(__dirname + '/dist'))
 
-app.listen(3000)
+app.listen(process.env.PORT)
